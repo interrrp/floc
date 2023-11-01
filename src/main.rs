6a36de850dd414a94bf139dda36aa9ecf56b5ca1
interrrp <int@addictedto.coffee>
2023-11-01 15:39:33 +0800

    Make processing parallel

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
+use std::sync::{atomic::AtomicU64, Arc};
+
+use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
+
 mod args;
 mod utils;
 
 fn main() {
     let args = args::parse();
 
-    let files = utils::find_files_with_extension(&args.directory, &args.extension);
-    let mut total = 0;
-    for file in files {
-        let lines = utils::count_lines(&file);
-        total += lines;
-        println!("{}: {} LoC", file.display(), lines);
-    }
+    let total = Arc::new(AtomicU64::new(0));
+    utils::find_files_with_extension(&args.directory, &args.extension)
+        .par_iter()
+        .for_each(|path| {
+            let lines = utils::count_lines(path);
+            total.fetch_add(lines as u64, std::sync::atomic::Ordering::SeqCst);
+            println!("{}: {} LoC", path.display(), lines);
+        });
+
+    let total = total.load(std::sync::atomic::Ordering::SeqCst);
 
     println!("Total: {} LoC", total);
 
