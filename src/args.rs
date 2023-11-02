7a389980a3c77836de7a2cd6bf76919ec6a7c7fe
interrrp <int@addictedto.coffee>
2023-11-02 18:12:50 +0800

    Make `DIRECTORY` an argument instead of an option

--- src/args.rs
+++ src/args.rs
@@ -5,13 +5,13 @@ use clap::Parser;
 /// Count the lines in your codebase.
 #[derive(clap::Parser)]
 pub struct Args {
+    /// The directory to check. Example: "src" or "tests".
+    #[clap(default_value = "src")]
+    pub directory: PathBuf,
+
     /// The file extension(s) to check. Example: "rs" or "py".
     #[clap(short, long, default_value = "rs")]
     pub extensions: Vec<String>,
-
-    /// The directory to check. Example: "src" or "tests".
-    #[clap(short, long, default_value = "src")]
-    pub directory: PathBuf,
 }
 
 pub fn parse() -> Args {
